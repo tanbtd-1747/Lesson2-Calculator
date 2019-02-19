58bcc17ba7de109c1c16d8ff46726d7b20405edb
Tran Duc Tan <tran.duc.tanb@framgia.com>
2019-02-19 08:47:11 +0700

    Initial Commit

---
+++ L2-Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  L2-Calculator
+//
+//  Created by tran.duc.tan on 2/19/19.
+//  Copyright © 2019 tran.duc.tanb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
